0123cce7b00fa140493f30812046492fe1103e1b
brunnseb <brunnseb@students.zhaw.ch>
2017-03-21 10:12:32 +0100

    finished exercise 2 and renamed ex.3

--- p3/Brunner_Sebastian_G12_S3_Aufg3.m
+++ p3/Brunner_Sebastian_G12_S3_Aufg3.m
@@ -36,9 +36,28 @@ while mult ~= 0 && count < nmax,
     dummy3(count)=num2str(fix(mult));
     mult=(mult-fix(mult))*B;
 end
-%Ausgabe als y inklusive Vorzeichen und Punkt.
-fprintf('%s.%s\n', dummy2,dummy3);
-% Berechnung des Fehlers
+format short e
+value=x;
+valuestring=strcat('value = %0.',num2str(nmax),'e\n');
 
+format long
+fprintf(valuestring,value);
+y=strcat(num2str(sign(x)*str2num(dummy2)),'.',dummy3);  
+%Ausgabe als y inklusive Vorzeichen und Punkt.
+fprintf('y = %s\n', y);
+% Berechnung des Fehlers
+length=numel(dummy2);
+dummy4=0;
+dummy5=0;
+for i=1:length,
+    dummy4=dummy4+str2num(dummy2(i))*B^(length-i);
+    dummy5=dummy5+str2num(dummy3(i))*B^(-i);
+end
+format long
+dummy4
+dummy5
+x
+abs(x)
+abs_err=(x-abs(x))
 end
 
