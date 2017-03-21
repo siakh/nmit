9c6a1273802caa875fcf60282869a096bea09da1
brunnseb <brunnseb@students.zhaw.ch>
2017-03-21 13:30:33 +0100

    started p4

---
+++ p4/Brunner_Sebastian_G12_S4_Aufg2.m
@@ -0,0 +1,7 @@
+function [ output_args ] = Brunner_Sebastian_G12_S4_Aufg2( input_args )
+%BRUNNER_SEBASTIAN_G12_S4_AUFG2 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
