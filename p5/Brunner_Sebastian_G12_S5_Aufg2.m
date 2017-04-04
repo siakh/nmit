fcfc54e5c3d0f0543a9b510907ea8d433d592570
brunnseb <brunnseb@students.zhaw.ch>
2017-04-04 14:50:11 +0200

    finished series 5

--- p5/Brunner_Sebastian_G12_S5_Aufg2.m
+++ p5/Brunner_Sebastian_G12_S5_Aufg2.m
@@ -1,10 +1,29 @@
+% Aufgabe 2a)
+
 k=zeros(50,1);
-alpha=[0:0.25:4]
+alpha=[0.25:0.25:4];
 for i=1:16,
     k(1)=0.1;
     alphaI=alpha(i);
     for j=1:50,
-        k(j+1)=alpha*k(j)*(1-k(j));
+        k(j+1)=alphaI*k(j)*(1-k(j));
+        
     end
-    plot();
+    subplot(4,4,i);
+    plot(k);
+    legend(strcat('alpha: ',num2str(alphaI)));
 end
+
+% Aufgabe 2b)
+% 
+% Fixpunkt = Konstante Rate von kranke Kinder (z.B. Fixpunkt = 0.3 -> Es sind immer 30% Kinder krank.
+%
+%
+% Aufgabe 2c)
+%
+% Fixpunktgleichung nach alpha auflösen:
+%
+% k=alpha*k(1-k)
+% alpha=1/(1-k)
+%
+% z.B. alpha = 2 -> k = 0.5, deckt sich mit dem plot aus Aufgabe 2a)
---
+++ p5/Brunner_Sebastian_G12_S5_Aufg3.m
@@ -0,0 +1,4 @@
+x=0:pi/10:2*pi;
+f=sin(x);
+g=-0.5*pi+x;
+plot(x,f,'r',x,g,'g')
